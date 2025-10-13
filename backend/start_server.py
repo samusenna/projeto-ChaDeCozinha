ef0467af6132c31eac885d2403302a6e0a8e3302
_samusenna <samuel.sena@sptech.scholl>
2025-10-13 15:46:13 -0300

    vercel.json no back

--- backend/start_server.py
+++ backend/start_server.py
@@ -2,7 +2,7 @@ import os
 import sys
 sys.path.insert(0, os.path.dirname(__file__))
 
-from src.main import app
+from backend.api.main import app
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001, debug=True)
